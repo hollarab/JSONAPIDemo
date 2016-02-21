2f8b5450fdd68a104ec97676b0540ef9e750be20
a. brooks hollar <hollarab@gmail.com>
2016-02-21 12:12:17 -0700

    ticket-42 added User phone in Playground

--- JSONTestPlay.playground/Contents.swift
+++ JSONTestPlay.playground/Contents.swift
@@ -1,9 +1,11 @@
 
 class User {
     var name:String
+    var phone:String
     
-    init(name:String) {
+    init(name:String, phone:String) {
         self.name = name
+        self.phone = phone
     }
 }
 
@@ -16,7 +18,7 @@ var users = [User]()
 
 for obj in userResponse.arrayValue {
     print(obj)
-    users.append(User(name:obj["name"].stringValue))
+    users.append(User(name:obj["name"].stringValue, phone:obj["phone"].stringValue))
 }
 
 users
